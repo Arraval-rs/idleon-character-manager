4f5d1c1e52e89426bc565c87ba5761094d3e9622
Arraval-rs <cody.coombs@live.ca>
2021-06-17 20:42:04 -0230

    json parsed into dictionary, initial window

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from tkinter import *
+from tkinter import ttk
 import json
+
+json_file = open("idleon_data.json", "rt")
+json_text = json_file.read()
+
+dictionary = json.loads(json_text)
+
+window = tk.Tk()
+
+window.mainloop()
