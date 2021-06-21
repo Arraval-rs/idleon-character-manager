9457d01c8fc221da998eef799c8ec9494edd6e41
Arraval-rs <cody.coombs@live.ca>
2021-06-21 17:38:38 -0230

    layout configured with PySimpleGUI

--- test.py
+++ test.py
@@ -30,22 +30,31 @@ foods_tab =         [
 
 characters_tab =    [
                         [
-                        sg.Text('Character 1\nLv. X'),
-                        sg.Text('Class\nImage'),
-                        sg.Text('Class Name'),
-                        sg.TabGroup([[
-                                        sg.Tab('Equips', equips_tab),
-                                        sg.Tab('Tools', tools_tab),
-                                        sg.Tab('Food', foods_tab)
-                                    ]])],
-                        [
-                        sg.Text("Blah Blah Stats and Shit"),
-                        sg.TabGroup([[
-                                        sg.Tab('Skills', skills_tab),
-                                        sg.Tab('Talents', talents_tab),
-                                        sg.Tab('Bags', bags_tab),
-                                        sg.Tab('Pouches', pouches_tab)
-                                    ]])
+                            sg.Column(
+                            [[
+                                sg.Text('Character 1\nLv. X'),
+                                sg.Text('Class\nImage'),
+                                sg.Text('Class Name'),
+                            ],
+                            [
+                                sg.Text("Blah Blah Stats and Shit"),
+                                sg.TabGroup(
+                                [[
+                                    sg.Tab('Skills', skills_tab),
+                                    sg.Tab('Talents', talents_tab),
+                                    sg.Tab('Bags', bags_tab),
+                                    sg.Tab('Pouches', pouches_tab)
+                                ]])
+                            ]]),
+                            sg.Column(
+                            [[
+                                sg.TabGroup(
+                                [[
+                                    sg.Tab('Equips', equips_tab),
+                                    sg.Tab('Tools', tools_tab),
+                                    sg.Tab('Food', foods_tab)
+                                ]])
+                            ]])
                         ]
                     ]
 
@@ -66,13 +75,14 @@ storage_tab =       [
                     ]
 root_tabs = [
                 [
-                sg.TabGroup([[
-                                sg.Tab('Characters', characters_tab),
-                                sg.Tab('Inventory', inventory_tab),
-                                sg.Tab('Monsters', monsters_tab),
-                                sg.Tab('Crafting', crafting_tab),
-                                sg.Tab('Storage', storage_tab)
-                            ]])
+                sg.TabGroup(
+                [[
+                    sg.Tab('Characters', characters_tab),
+                    sg.Tab('Inventory', inventory_tab),
+                    sg.Tab('Monsters', monsters_tab),
+                    sg.Tab('Crafting', crafting_tab),
+                    sg.Tab('Storage', storage_tab)
+                ]])
                 ]
             ]
 
