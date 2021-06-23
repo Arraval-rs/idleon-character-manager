e5a8cb824379a65028c7ce9d8bd2f722225c2ec2
Arraval-rs <cody.coombs@live.ca>
2021-06-23 15:05:35 -0230

    Added handling for missing images, need to add for empty slots

--- ICM.py
+++ ICM.py
@@ -6,6 +6,8 @@ from PIL import Image
 
 # Functions
 def generate_img(f): # returns the bytes object for a given image file
+    if not os.path.exists(f):
+        f = 'images/Missing.png'
     img = Image.open(f)
     img.thumbnail((72, 72))
     bio = io.BytesIO()
@@ -26,7 +28,7 @@ def is_base_class(c): # returns true if the given class is a base class (Beginne
         return True
     return False
 
-# Dictionary
+# Dictionary for JSON from Idleon API Downloader
 json_file = open("idleon_data.json", "rt")
 json_text = json_file.read()
 dictionary = json.loads(json_text)
@@ -43,7 +45,7 @@ while i < len(dictionary['characters']):
         class_name = dictionary['characters'][i]['class']))
     i = i + 1
 
-# Dictionary
+# Dictionary for talent images
 talents =   {
                 'Mage':{'Shaman':{}, 'Wizard':{}}, 
                 'Warrior':{'Barbarian':{}, 'Squire':{}}, 
@@ -159,7 +161,7 @@ talents_2 =         [[
                             [sg.Image(data = talents['Filler'].getvalue() if character_base_class == 'Beginner' else talents[character_base_class]['18'].getvalue(), key = 'talent_img18')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['18']), key = 'talent18', size = (7,1), justification = 'center')],
                             [sg.Image(data = talents['Filler'].getvalue() if character_base_class == 'Beginner' else talents[character_base_class]['23'].getvalue(), key = 'talent_img23')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent23', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent23', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if character_base_class == 'Beginner' else talents[character_base_class]['28'].getvalue(), key = 'talent_img28')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['28']), key = 'talent28', size = (7,1), justification = 'center')]
                         ], element_justification = 'center'),
@@ -190,7 +192,7 @@ talents_3 =         [[
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['31'].getvalue(), key = 'talent_img31')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['31']), key = 'talent31', size = (7,1), justification = 'center')],
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['36'].getvalue(), key = 'talent_img36')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent36', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent36', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['41'].getvalue(), key = 'talent_img41')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['41']), key = 'talent41', size = (7,1), justification = 'center')]
                         ], element_justification = 'center'),
@@ -199,25 +201,25 @@ talents_3 =         [[
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['32'].getvalue(), key = 'talent_img32')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['32']), key = 'talent32', size = (7,1), justification = 'center')],
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['37'].getvalue(), key = 'talent_img37')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent37', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent37', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['42'].getvalue(), key = 'talent_img42')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['42']), key = 'talent42', size = (7,1), justification = 'center')]
                         ], element_justification = 'center'),
                         sg.Column(
                         [
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['33'].getvalue(), key = 'talent_img33')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent33', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent33', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['38'].getvalue(), key = 'talent_img38')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent38', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent38', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['43'].getvalue(), key = 'talent_img43')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['43']), key = 'talent43', size = (7,1), justification = 'center')]
                         ], element_justification = 'center'),
                         sg.Column(
                         [
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['34'].getvalue(), key = 'talent_img34')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent34', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent34', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['39'].getvalue(), key = 'talent_img39')],
-                            [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['0']), key = 'talent39', size = (7,1), justification = 'center')], # MISSING FROM JSON
+                            [sg.Text('{}/100'.format(0), key = 'talent39', size = (7,1), justification = 'center')], # MISSING FROM JSON
                             [sg.Image(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class]['44'].getvalue(), key = 'talent_img44')],
                             [sg.Text('{}/100'.format(dictionary['characters'][0]['talentLevels']['44']), key = 'talent44', size = (7,1), justification = 'center')]
                         ], element_justification = 'center')
@@ -379,18 +381,18 @@ while True:
             if str(i) in dictionary['characters'][index]['talentLevels'].keys(): # some talents aren't in JSON
                 window['talent{}'.format(i)].update('{}/100'.format(dictionary['characters'][index]['talentLevels'][str(i)]))
             else:
-                window['talent{}'.format(i)].update('?/100')
+                window['talent{}'.format(i)].update('0/100')
         for i in range(10, 30):
             window['talent_img{}'.format(i)].update(data = talents['Filler'].getvalue() if character_base_class == 'Beginner' else talents[character_base_class][str(i)].getvalue())
             if str(i) in dictionary['characters'][index]['talentLevels'].keys(): # some talents aren't in JSON
                 window['talent{}'.format(i)].update('{}/100'.format(dictionary['characters'][index]['talentLevels'][str(i)]))
             else:
-                window['talent{}'.format(i)].update('?/100')
+                window['talent{}'.format(i)].update('0/100')
         for i in range(30, 45):
             window['talent_img{}'.format(i)].update(data = talents['Filler'].getvalue() if is_base_class(character_class) else talents[character_base_class][character_class][str(i)].getvalue())
             if str(i) in dictionary['characters'][index]['talentLevels'].keys(): # some talents aren't in JSON
                 window['talent{}'.format(i)].update('{}/100'.format(dictionary['characters'][index]['talentLevels'][str(i)]))
             else:
-                window['talent{}'.format(i)].update('?/100')
+                window['talent{}'.format(i)].update('0/100')
 
 window.close()       
